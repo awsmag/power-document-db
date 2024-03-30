1e9de6a29e684de533a1184faca7cb2ad43fcd19
Simranjeet Singh <simranjeet@s25.digital>
2024-03-30 10:20:34 +0530

    updating types

--- types/client.d.ts
+++ types/client.d.ts
@@ -1,5 +1,21 @@
-import { MongoClient, Db } from "mongodb";
+import { MongoClient, Db, ClientSession } from "mongodb";
+
+type SessionParams = {
+  causalConsistency: boolean;
+  snapshot: boolean;
+};
+
 export declare function connectDb(uri: string, ssl: boolean): Promise<void>;
-export declare function getDbClient(uri: string, ssl: boolean): Promise<MongoClient>;
-export declare function getConfiguredDb(uri?: string, ssl?: boolean, dbName?: string): Promise<Db>;
+export declare function getDbClient(
+  uri: string,
+  ssl: boolean,
+): Promise<MongoClient>;
+export declare function getConfiguredDb(
+  uri?: string,
+  ssl?: boolean,
+  dbName?: string,
+): Promise<Db>;
 export declare function closeDbConnection(): Promise<void>;
+export declare function startSesion(
+  params?: SessionParams,
+): Promise<ClientSession>;
