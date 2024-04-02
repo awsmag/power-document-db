b3f7baa91f30aa7cf672edb607a04e90f8042bc0
Simranjeet Singh <simranjeet@s25.digital>
2024-04-02 13:22:29 +0530

    [fixing types for start session]

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,6 +1,7 @@
-import { getConfiguredDb as getCDb, closeDbConnection as clDb } from "./client";
+import { getConfiguredDb as getCDb, closeDbConnection as clDb, startSesion as st } from "./client";
 export * from "./koa-mw";
 export declare function connectDb(uri?: string, dbName?: string, ssl?: boolean): Promise<import("mongodb").Db>;
 export declare const getConfiguredDb: typeof getCDb;
 export declare const closeDbConnection: typeof clDb;
+export declare const startSesion: typeof st;
 export * from "mongodb";
