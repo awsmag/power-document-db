66c025bf02afa19b001834d17806a111bf5211b7
Simranjeet Singh <simranjeet@s25.digital>
2024-04-12 19:22:50 +0530

    [Fixing session]

--- src/client.ts
+++ src/client.ts
@@ -1,31 +1,30 @@
 import { MongoClient, Db, MongoClientOptions, ClientSession } from "mongodb";
-import path from "path";
 
 let client: MongoClient;
 let db: Db;
 
-export async function connectDb(uri: string, ssl: boolean, tlsCAFile: string = "") {
-  try {
-    if (!uri) {
-      throw new Error("Connection String and DB Name are required");
-    }
-
-    if (ssl === true && !tlsCAFile) {
-      throw new Error("CA File path required for ssl connection");
-    }
-
-    const options: MongoClientOptions =
-      ssl === false
-        ? {}
-        : {
-            tlsCAFile: tlsCAFile,
-            tls: true,
-          };
-
-    client = await MongoClient.connect(uri, options);
-  } catch (error) {
-    throw error;
+export async function connectDb(
+  uri: string,
+  ssl: boolean,
+  tlsCAFile: string = "",
+) {
+  if (!uri) {
+    throw new Error("Connection String and DB Name are required");
   }
+
+  if (ssl === true && !tlsCAFile) {
+    throw new Error("CA File path required for ssl connection");
+  }
+
+  const options: MongoClientOptions =
+    ssl === false
+      ? {}
+      : {
+          tlsCAFile: tlsCAFile,
+          tls: true,
+        };
+
+  client = await MongoClient.connect(uri, options);
 }
 
 export async function getDbClient(uri: string, ssl: boolean, tlsCAFile = "") {
@@ -39,7 +38,7 @@ export async function getConfiguredDb(
   uri: string = "",
   ssl: boolean = false,
   dbName: string = "",
-  tlsCAFile: string = ""
+  tlsCAFile: string = "",
 ): Promise<Db> {
   if (!db) {
     const client = await getDbClient(uri, ssl, tlsCAFile);
@@ -59,9 +58,9 @@ export async function startSession(
     throw new Error("Not connected");
   }
 
-  return await client.startSession({
+  return client.startSession({
     causalConsistency,
-    snapshot
+    snapshot,
   });
 }
 
--- types/index.d.ts
+++ types/index.d.ts
@@ -1,6 +1,6 @@
 import { getConfiguredDb as getCDb, closeDbConnection as clDb, startSession as st } from "./client";
 export * from "./koa-mw";
-export declare function connectDb(uri?: string, dbName?: string, ssl?: boolean): Promise<import("mongodb").Db>;
+export declare function connectDb(uri?: string, dbName?: string, ssl?: boolean, tlsCAFile?: string): Promise<import("mongodb").Db>;
 export declare const getConfiguredDb: typeof getCDb;
 export declare const closeDbConnection: typeof clDb;
 export declare const startSession: typeof st;
